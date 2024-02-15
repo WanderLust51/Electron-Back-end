502ebca9207d2096d4144cb27b22eb397c52ccad
Andrés Lugo <andreslugo51@gmail.com>
2024-02-15 14:46:08 -0400

    tercera clase de Electron

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 const {app} = require('electron')
+const {BrowserWindow} = require('electron')
+const {dialog} = require('electron')
 
 console.log(app.name)
 
 app.on('ready',()=>{
     console.log('La app está lista vro')
+
+    win = new BrowserWindow({
+        x: 0,
+        y: 0,
+        closable: true
+    }).loadFile('index.html')
+
+    setTimeout(()=>{
+        // app.exit()
+        resp = dialog.showMessageBoxSync(win,{
+            title: 'Saludos',
+            message: 'Hola saludos',
+            buttons : ['Hola', 'Chao']
+        })
+        
+    }, 5000)
 })
