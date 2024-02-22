f686d2e65683c88b909dfa99e6a429d9a87d96ae
Andrés Lugo <andreslugo51@gmail.com>
2024-02-22 14:42:15 -0400

    cuarta clase de Electron (menu de opciones)

--- main.js
+++ main.js
@@ -1,25 +1,37 @@
 const {app} = require('electron')
 const {BrowserWindow} = require('electron')
-const {dialog} = require('electron')
+const {dialog, Menu} = require('electron')
 
-console.log(app.name)
+const {plantilla} = require("./menu.js")
+let myMenu = Menu.buildFromTemplate(plantilla)
 
 app.on('ready',()=>{
-    console.log('La app está lista vro')
+    console.log('La app esta lista vro')
 
     win = new BrowserWindow({
         x: 0,
         y: 0,
-        closable: true
-    }).loadFile('index.html')
+        closable: true,
+        webPreferences: {
+            // preload: path.join('script.js')
+        }
+    })
+    win.loadFile('index.html')
+    win.setMenu(myMenu)
 
     setTimeout(()=>{
         // app.exit()
         resp = dialog.showMessageBoxSync(win,{
             title: 'Saludos',
             message: 'Hola saludos',
+            noLink: true,
             buttons : ['Hola', 'Chao']
         })
+        if (resp == 1){
+            if (!win.isDestroyed()){
+                BrowserWindow.getAllWindows()[0].close()
+            }
+        }
         
-    }, 5000)
+    }, 3000)
 })
---
+++ menu.js
@@ -0,0 +1,24 @@
+module.exports.plantilla =
+[
+    {
+        label:"Connection",
+        click: ()=>{
+            console.log("click en connection")
+        }
+    },
+    {
+        label: "Mode",
+        click: ()=>{
+            console.log("click en Mode")
+        }
+    },
+    {
+        label:"View"
+    },
+    {
+        label:"Tools"
+    },
+    {
+        label:"Help"
+    }
+] 
